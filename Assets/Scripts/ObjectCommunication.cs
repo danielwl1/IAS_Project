538792f5d96df80cf2dc4fc5822b5d845451f1b1
danielwl1 <danielwl1@outlook.de>
2020-07-02 13:17:09 +0200

    Prototype2
    second iteration of my Augmeted Toy Prototype

---
+++ Assets/Scripts/ObjectCommunication.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ObjectCommunication : MonoBehaviour
+{
+    private bool  startInteraction;
+
+    Animator animator;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startInteraction = false;
+        animator = GetComponent<Animator>();
+        animator.Play("Obj_Idle");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startInteraction)
+        {
+            animator.Play("Obj_Speak");
+
+        }
+        else {
+            animator.Play("Obj_Idle");
+        }
+    }
+
+    public void StartMoving() {
+        startInteraction = true;
+        
+    }
+    public void StopMoving() {
+        startInteraction = false;
+    }
+
+    
+}
---
+++ Assets/Scripts/RabbitMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitMovement : MonoBehaviour
+{
+    private bool enableMovement = false; 
+    public void Update()
+    {
+        if (enableMovement)
+        transform.position += transform.forward * Time.deltaTime;
+    }
+
+    public void StartMovement() {
+        print("ich bin dumm");
+        enableMovement = true;
+    }
+}
